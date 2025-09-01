b3a9ef7f0ea460404f6ba085667dfa17e13614e3
Rishabh Kamboj <heyrishabh13@gmail.com>
2025-09-01 17:30:59 +0530

    getElementByTagName folder added

---
+++ getElementByTagName/index.js
@@ -0,0 +1,6 @@
+const listItems = document.getElementsByTagName("li");
+for (let i = 0; i < listItems.length; i++) {
+  listItems[i].style.fontStyle = "italic";
+}
+
+document.getElementById("mango").style.color = "blue";
